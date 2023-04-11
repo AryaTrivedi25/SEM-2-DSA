63b1eb8c5acd6181455d8b5ebe03f62b03a93b24
Arya Trivedi <113917940+AryaTrivedi25@users.noreply.github.com>
2023-04-11 22:41:15 +0530

    Add files via upload
    creating a doubly linked list

---
+++ DoublyLinkedList.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node * pre;
+    int data;
+    struct node * next;
+};
+
+int main()
+{
+    struct node * head = (struct node *)malloc(sizeof(struct node));
+    head->pre = NULL;
+}
