0cce95d7e199fcb41d4326063684ae08eadfa5fb
Arya Trivedi <113917940+AryaTrivedi25@users.noreply.github.com>
2023-04-18 23:58:11 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[3]={4,3};
+    //inserting:
+    printf("enter the third value of array:\n");
+    scanf("%d", &arr[2]);
+
+    //deleting  and searching;
+    int n;
+    printf("enter the value to delete:\n");
+    scanf("%d", &n);
+
+    int i;
+    for( i =0 ;i<3;i++)
+    {
+        if(arr[i]==n)
+        break;
+    }
+
+    for(int j=i ;j<3-1;j++ )
+    {
+     arr[j] = arr[j+1];
+    }
+
+     //update:
+     arr[0]=1;
+
+     //traverse:
+
+     for(int k =0 ;k<2 ; k++)
+     {
+        printf("%d index contains : %d\n",k,arr[k]);
+     }
+}
